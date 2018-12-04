ff6ac1e1464ed3f29408fc9804ba887913499613
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-04 18:03:24 +0400

    add generic converter

---
+++ OOP.Generic.UniversalConverter/OOP.Generic.UniversalConverter/Converter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+
+namespace UniversalConverter
+{
+    class Converter<T>where T:struct
+    {
+       
+        public  T? StringConvert( string inputValue) 
+        {
+            T? result = null;//T cann be  Nullable<T> type
+           
+            switch (Type.GetTypeCode(typeof(T)))
+            {
+                case TypeCode.Boolean:
+                    result = (T)(object)Convert.ToBoolean(inputValue);
+                    break;
+                case TypeCode.Int16:
+                    result = (T)(object)char.Parse(inputValue);
+                    break;
+                case TypeCode.Int32:
+                    result = (T)(object)Convert.ToInt32(inputValue);
+                    break;
+                case TypeCode.Int64:
+                    result = (T)(object)long.Parse(inputValue);
+                    break;
+                case TypeCode.Single:
+                    result = (T)(object)Convert.ToSingle(inputValue);
+                    break;
+                case TypeCode.Double:
+                    result = (T)(object)Convert.ToDouble(inputValue);
+                    break;
+                case TypeCode.Decimal:
+                    result = (T)(object)Convert.ToDecimal(inputValue);
+                    break;
+                default:
+                    throw new Exception("Conversion error,no suitable type");
+            }
+            return result;
+        }
+
+     
+    }
+}
--- OOP.Generic.UniversalConverter/OOP.Generic.UniversalConverter/Program.cs
+++ OOP.Generic.UniversalConverter/OOP.Generic.UniversalConverter/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace OOP.Generic.UniversalConverter
+namespace UniversalConverter
 {
     class Program
     {
+        //գրել ունիվերսալ կոնվերտեր որը կփոխի string-ը ձեր ուզած տիպի
+      
+        public static object ChooseTypeForConvert<T>(Converter<T> converter)where T:struct
+        {
+            object result = null;
+            Console.Write("Input string for convert: ");
+           string input = Console.ReadLine();
+            result = converter.StringConvert(input);
+            return result;
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Converter<int> converter = new Converter<int>();
+           
+            try
+            {
+                Console.WriteLine(ChooseTypeForConvert(converter));
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentException arg)
+            {
+                Console.WriteLine(arg.Message);
+            }
+            catch (FormatException fm)
+            {
+                Console.WriteLine(fm.Message);
+            }
+            catch (OverflowException ov)
+            {
+                Console.WriteLine(ov.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
