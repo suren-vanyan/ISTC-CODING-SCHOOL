18437cd3fc62f8cce86314b9b3c4e37fc973f833
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-04 14:57:13 +0400

    first commit

---
+++ OOP.Generic.UniversalConverter/OOP.Generic.UniversalConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP.Generic.UniversalConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
