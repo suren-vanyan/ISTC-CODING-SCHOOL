6028586b085478fa1992fd0a33944e059422f59c
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-09 18:09:41 +0400

    Operator Overloading

---
+++ OOP.OperatorOverloading.Task6/OOP.OperatorOverloading.Task6/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP.OperatorOverloading.Task6
+{
+    struct Point
+    {
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+
+        public Point(int XCoord, int YCoord)
+        {
+            this.XCoord = XCoord;
+            this.YCoord = YCoord;
+        }
+
+        public static Point operator +(Point p1, Point p2) { return new Point(p1.XCoord + p2.XCoord, p1.YCoord + p2.YCoord); }
+        public static Point operator +(Point p1, int change) { return new Point(p1.XCoord + change, p1.YCoord + change); }
+
+
+        public static Point operator -(Point p1, Point p2) { return new Point(p1.YCoord - p2.XCoord, p1.YCoord - p2.YCoord); }
+        public static Point operator -(Point p1, int change) { return new Point(p1.YCoord - change, p1.YCoord - change); }
+
+
+        public static Point operator ++(Point p) { return new Point(p.XCoord+1 , ++p.YCoord); }//need prefix increment or +1
+        public static Point operator --(Point p) { return new Point(--p.XCoord, --p.YCoord); }//need prefix increment or -1
+
+
+        public override string ToString() { return $"[{this.XCoord},{this.YCoord}]"; }
+
+    }
+}
---
+++ OOP.OperatorOverloading.Task6/OOP.OperatorOverloading.Task6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OOP.OperatorOverloading.Task6
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Point pointOne = new Point(new Random().Next(1,20),new Random().Next(1,30));
+            Point pointTwo = new Point(new Random().Next(1, 40), new Random().Next(1, 50));
+           // Print(pointOne, pointTwo);
+
+           //+
+            Point additionPoints = pointOne + pointTwo;
+            additionPoints = additionPoints + 2;
+
+            var point = new Point
+            {
+                XCoord = pointOne.XCoord + pointTwo.XCoord,
+                YCoord = pointOne.YCoord + pointTwo.YCoord
+
+            };
+
+            //-
+            Point subtractPoints = pointOne - pointTwo;
+            subtractPoints = subtractPoints - 2;
+
+
+            // In terms of C#, the shorthand assignment operators are automatically simulated if a type overloads the related binary operator.
+            //+=
+            Point ShortAssOpOne = pointOne += pointTwo;
+            //-=
+            Point ShortAssOptwo = pointOne += pointTwo;
+
+
+
+            //Unary Operators  --
+            Point incrementOp = pointOne++;
+            //++
+            Point decrementOp = pointTwo--;
+        }
+    }
+}
