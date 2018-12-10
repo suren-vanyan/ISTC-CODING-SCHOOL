56c04da74b525eafa97bea44770af34d176b0b4a
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-10 13:01:26 +0400

    Extension Metod

---
+++ OOP.Extension Methods.Task8/OOP.Extension Methods.Task8/MyExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MyExtensionMethods
+{
+    static class MyExtensions
+    {
+        
+        //the first parameter of an extension method represents the type being extended.
+        public static int ReverseNumber(this int i)
+        {
+            char[] digits = i.ToString().ToCharArray();
+            Array.Reverse(digits);
+
+            string newDigits = new string(digits);
+            int result = Convert.ToInt32(newDigits);
+            return result;
+        }
+
+        public static int ConvertToInt(this string str)
+        {
+            bool result = int.TryParse(str, out int x);
+            x = result ? x : int.MaxValue;
+            return x;
+        }
+        public static void Print(this IEnumerable collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+              
+            }
+        }
+
+
+    }
+}
--- OOP.Extension Methods.Task8/OOP.Extension Methods.Task8/Program.cs
+++ OOP.Extension Methods.Task8/OOP.Extension Methods.Task8/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace OOP.Extension_Methods.Task8
 {
+    //It is common practice to not only isolate extension methods into a dedicated.NET namespace but to
+    //isolate them into a dedicated class library. In this way, new applications can “opt in” to extensions by explicitly
+    //referencing the correct library and importing the namespace.====>
+
+    using MyExtensionMethods;
+    class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name=>{FirstName},SurName=>{LastName},Age=>{Age}";
+        }
+    }
+
+    class Demonstration : IEnumerable<Person>
+    {
+        Person[] people = new Person[]
+        {
+            new Person{FirstName="Suren",LastName="Vanyan",Age=25},
+             new Person{FirstName="Antony ",LastName="Joshua",Age=29},
+             new Person{FirstName="Deontey ",LastName="Wilder",Age=33},
+
+        };
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+            foreach (var item in people)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var item in people)
+            {
+                yield return item;
+            }
+        }
+    }
+
     class Program
     {
+     
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string[] data = { "I", "Love", "You", "Babe", "of", "(^_^)" };
+            data.Print();
+
+            Demonstration demo = new Demonstration();
+            demo.Print();
+
+            int x = 123456789;
+            x = x.ReverseNumber();
+            Console.WriteLine(x);
+
+            Console.Write("Number:");
+            string str = Console.ReadLine() + Environment.NewLine;//enter number
+            int result = str.ConvertToInt();
+            Console.WriteLine("Result:" + result);
         }
     }
 }
